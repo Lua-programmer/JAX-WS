1b010606aeab580464ff9c2022990cb86489345f
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-03 20:24:26 -0300

    Consumindo serviço baseado em SOAP

--- jax-ws-c/src/main/java/org/example/App.java
+++ jax-ws-c/src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import jakarta.xml.ws.Service;
+
+import javax.xml.namespace.QName;
+import java.net.URL;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://localhost:8085/servico/certidao?wsdl");
+
+        QName qname = new QName("http://service.example.org/", "CertidaoNascimentoImplService");
+
+        Service service = Service.create(url, qname); //comunicação com o serviço que vamos consumir
+
+        CertidaoNascimento certidao = service.getPort(CertidaoNascimento.class);
+        int idade = certidao.calcularIdade("16/08/1996");
+        String diaSemana = certidao.diaSemanaNascimento("16/08/1996");
+        System.out.println("Idade: " + idade);
+        System.out.println("Dia da semana: " + diaSemana);
     }
 }
